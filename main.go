0308f7780c0cb1da755cb0d7cf2b8f7e928580f6
artem potapov <zoomskij@gmail.com>
2017-08-20 16:08:53 +0300

    added /today and estimate

--- main.go
+++ main.go
@@ -1,39 +1,46 @@
 package main
+
 import (
-  "github.com/Syfaro/telegram-bot-api"
-  "github.com/andygrunwald/go-jira"
-  "log"
-  "fmt"
+	"log"
+	"strconv"
+
+	"github.com/Syfaro/telegram-bot-api"
+	"github.com/andygrunwald/go-jira"
 )
 
 func main() {
-  jiraClient, err := jira.NewClient(nil, "https://jira.instance.com")
+	jiraClient, err := jira.NewClient(nil, "https://your-instance.com")
 	if err != nil {
 		panic(err)
 	}
-	jiraClient.Authentication.SetBasicAuth("username", "passowrd")
+	jiraClient.Authentication.SetBasicAuth("username", "password")
 
-	issue, _, err := jiraClient.Issue.Get("TASK-KEY", nil)
+	//boards :=
+	//issue, _, err := jiraClient.Issue.Get("SAM-33", nil)
+	issue, _, err := jiraClient.Issue.Get("ISSUE-ID", nil)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
+	//boards := jiraClient.Board.GetBoard(boardID)
+	//board := jiraClient.Issue.Get("SAM-33", nil)
 
-  bot, err := tgbotapi.NewBotAPI("TOKEN")
-  if err != nil {
-    log.Panic(err)
-  }
+	//fmt.Printf("%s: %+v\n", issue.Key, issue.Fields.Summary)
+
+	bot, err := tgbotapi.NewBotAPI("TOKEN")
+	if err != nil {
+		log.Panic(err)
+	}
 
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-  //Initialize chanel
+	//Initialize chanel
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	upd, _ := bot.GetUpdatesChan(ucfg)
 
-  //Reading the updates from chanel
+	//Reading the updates from chanel
 	for {
 		select {
 		case update := <-upd:
@@ -44,13 +51,19 @@ func main() {
 
 			Text := update.Message.Text
 
-			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+			OriginalEstimate := strconv.Itoa(issue.Fields.TimeOriginalEstimate / 60)
+			TimeSpent := strconv.Itoa(issue.Fields.TimeSpent / 60)
+			TimeEstimate := strconv.Itoa(issue.Fields.TimeEstimate / 60)
 
-			reply := issue.Fields.Summary
+			if Text == "/today" {
+				log.Printf("[%s] %d %s", UserName, ChatID, Text)
 
-			msg := tgbotapi.NewMessage(ChatID, reply)
+				reply := issue.Fields.Summary + "(" + OriginalEstimate + "/" + TimeSpent + "/" + TimeEstimate + ")"
 
-			bot.Send(msg)
+				msg := tgbotapi.NewMessage(ChatID, reply)
+
+				bot.Send(msg)
+			}
 		}
 	}
 }
